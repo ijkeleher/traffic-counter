cdd124e084bacf4de6e2631662bc475d65d7e653
Isaac Keleher <isaac.keleher@gmail.com>
2021-05-01 12:29:05 +1000

    wip: skeleton code

--- counter.py
+++ counter.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+
+def read_file():
+    pass
+
+def count_total_cars():
+    pass
+
+def count_daily_cars():
+    pass
+
+def count_most_cars():
+    pass
+
+def count_least_cars():
+    pass
+
+"""
+outputs:
+total num of cars seen
+yyyy-mm-dd and num cars seen on that day for all cars in input file
+top 3 half hours with most cars in same format as input file
+the 1.5 hour period with least cars, i.e. 3 contiguous half hour records
+"""
+if __name__ == "__main__"():
+    pass
