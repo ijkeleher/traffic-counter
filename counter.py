0e65bfb0be433d43cfdb32e82937a19c0bf58b7b
Isaac Keleher <isaac.keleher@gmail.com>
2021-05-01 13:07:06 +1000

    feat: taking traffic log as input

--- counter.py
+++ counter.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
+import os
+import sys
 
-def read_file():
-    pass
+"""You can probably guess what this does by the name.
+
+Args:
+    fname (str): a filename
+Returns:
+    log: file contents of recorded traffic log
+"""
+def read_file(fname) -> str:
+    __location__ = os.path.realpath(
+    os.path.join(sys.path[0], fname))
+    log = open(fname, 'r')
+    return log.read()
 
 def count_total_cars():
     pass
@@ -22,5 +34,13 @@ yyyy-mm-dd and num cars seen on that day for all cars in input file
 top 3 half hours with most cars in same format as input file
 the 1.5 hour period with least cars, i.e. 3 contiguous half hour records
 """
-if __name__ == "__main__"():
-    pass
+if __name__ == "__main__":
+
+    if sys.argv[1]:
+        fname = sys.argv[1]
+    else:
+        fname = 'traffic.log'
+    
+    log = read_file(fname)
+
+    print(log)
